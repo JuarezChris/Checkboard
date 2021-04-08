364cdaae0f53c9ae4c420025131775784edc399a
Chris J <juarez.christopher@yahoo.com>
2021-04-08 17:09:16 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/<int:x>/<int:y>")
+def index(x, y):
+    return render_template("index.html", num1=x, num2=y)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
